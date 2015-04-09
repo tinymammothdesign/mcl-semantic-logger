e45b7d96ce01b5bf2574b257d57e9fd7a63ef399
Tiny Mammoth Design <developer@tinymammothdesign.com.au>
2015-04-09 15:50:30 +1000

    Converted Logger to use Monolog

--- SemanticLogger/Logger/Logger.php
+++ SemanticLogger/Logger/Logger.php
@@ -2,18 +2,27 @@
 
 namespace SemanticLogger\Logger;
 
+use Monolog\Handler\StreamHandler;
 use SemanticLogger\LogEvent\AbstractLogEvent;
 
 class Logger{
+
+    /**
+     * @var \Monolog\Logger
+     */
+    private $log;
 
     private $filePath;
 
     public function __construct($filePath){
         $this->filePath = $filePath;
+
+        $this->log = new \Monolog\Logger("Monolog");
+        $this->log->pushHandler(new StreamHandler($filePath, \Monolog\Logger::DEBUG));
     }
 
     public function persist(AbstractLogEvent $logEvent){
         $json = json_encode($logEvent->serialize());
-        file_put_contents($this->filePath, $json, FILE_APPEND);
+        $this->log->log(\Monolog\Logger::DEBUG, $json);
     }
 }
